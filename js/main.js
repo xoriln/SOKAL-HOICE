be3d833a77b81c145ff29240d7c41feb45b11d26
Vlad <vladislavsolyara@gmail.com>
2021-03-24 17:42:09 +0300

    section__about, slider

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(function(){
     $('.top-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="./images/next-arrow.svg" alt="next arrow"></button>',
-        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="./images/prev-arrow.svg" alt="prev arrow"></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="./images/next-arrow.svg" alt="next arrow" class="arrow next"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="./images/prev-arrow.svg" alt="prev arrow" class="arrow prev"></button>',
         responsive: [
           {
             breakpoint: 1024,
@@ -22,4 +22,15 @@ $(function(){
           }
         ]
       });
+      $(animation).ready(function(){
+        $("#animation").on("click","a", function (event) {
+          event.preventDefault();
+          var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+          $('body,html').animate({scrollTop: top}, 1500);
+        });
+      });
 });
+
+
+  
