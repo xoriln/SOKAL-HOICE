be072707db936e17340fc5a128d6d7490e1faf0e
Vlad <vladislavsolyara@gmail.com>
2021-03-30 10:31:36 +0300

    animation

--- js/main.js
+++ js/main.js
@@ -22,15 +22,13 @@ $(function(){
           }
         ]
       });
-      $(animation).ready(function(){
-        $("#animation").on("click","a", function (event) {
-          event.preventDefault();
-          var id  = $(this).attr('href'),
-          top = $(id).offset().top;
-          $('body,html').animate({scrollTop: top}, 1500);
-        });
-      });
 });
 
-
-  
+$(document).ready(function(){
+  $("header").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
