58e3c481e69e3c5e9f8779f1149a6cae12a86ef0
Vlad <vladislavsolyara@gmail.com>
2021-04-06 09:39:39 +0300

    contact form animation

--- js/main.js
+++ js/main.js
@@ -32,3 +32,13 @@ $(document).ready(function(){
       $('body,html').animate({scrollTop: top}, 1500);
   });
 });
+
+$(document).ready(function(){
+  $("footer").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
+
